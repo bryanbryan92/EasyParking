3d54e4b196356a279a673b8e8a41a711cf7a3946
bryanbryan92 <breinah@unal.edu.co>
2021-09-21 20:03:21 -0500

    Creacion clase Registro y eliminacion clase Contacto del paquete Logica

---
+++ src/main/java/logica/Registro.java
@@ -0,0 +1,49 @@
+
+package logica;
+
+
+public class Registro {
+    private String placa;
+    private String modelo;
+    private String color;
+    private String vehiculo;
+
+    public Registro() {
+        
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(String vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+    
+    
+    
+}
